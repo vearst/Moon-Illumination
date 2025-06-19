d18fa14a04a1fde18f2fa6272542ef8ac8c30a36
vearst <ommergo@gmail.com>
2025-06-19 12:19:56 +0300

    feat/use-singleQuote

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import "./App.css";
-import Gantt from "./Gantt";
+import React from 'react';
+import './App.css';
+import Gantt from './Gantt';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <Gantt />
       </header>
     </div>
--- frontend/src/Gantt/index.tsx
+++ frontend/src/Gantt/index.tsx
@@ -1,18 +1,18 @@
-import { ComponentRef, memo, useEffect, useRef } from "react";
-import "dhtmlx-gantt/codebase/dhtmlxgantt.css";
-import { gantt } from "dhtmlx-gantt";
-import { Stack } from "@mui/material";
+import { ComponentRef, memo, useEffect, useRef } from 'react';
+import 'dhtmlx-gantt/codebase/dhtmlxgantt.css';
+import { gantt } from 'dhtmlx-gantt';
+import { Stack } from '@mui/material';
 
 type GanttProps = {};
 
 const Gantt = memo<GanttProps>(() => {
-  const ganttContainer = useRef<ComponentRef<"div">>(null);
+  const ganttContainer = useRef<ComponentRef<'div'>>(null);
 
   useEffect(() => {
-    gantt.config.xml_date = "%Y-%m-%d";
-    gantt.config.scale_unit = "month";
-    gantt.config.date_scale = "%F %Y";
-    gantt.config.subscales = [{ unit: "day", step: 1, date: "%j %D" }];
+    gantt.config.xml_date = '%Y-%m-%d';
+    gantt.config.scale_unit = 'month';
+    gantt.config.date_scale = '%F %Y';
+    gantt.config.subscales = [{ unit: 'day', step: 1, date: '%j %D' }];
 
     if (ganttContainer.current) gantt.init(ganttContainer.current);
 
@@ -20,22 +20,22 @@ const Gantt = memo<GanttProps>(() => {
       data: [
         {
           id: 1,
-          text: "Project start",
-          start_date: "2025-06-19",
+          text: 'Project start',
+          start_date: '2025-06-19',
           duration: 5,
           progress: 0.6,
         },
         {
           id: 2,
-          text: "Development",
-          start_date: "2025-06-24",
+          text: 'Development',
+          start_date: '2025-06-24',
           duration: 8,
           progress: 0.3,
         },
         {
           id: 3,
-          text: "Testing",
-          start_date: "2025-07-03",
+          text: 'Testing',
+          start_date: '2025-07-03',
           duration: 4,
           progress: 0.1,
         },
@@ -50,7 +50,7 @@ const Gantt = memo<GanttProps>(() => {
   return (
     <Stack
       ref={ganttContainer}
-      style={{ width: "100%", height: "100vh" }}
+      style={{ width: '100%', height: '100vh' }}
     ></Stack>
   );
 });
